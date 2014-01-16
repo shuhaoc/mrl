c650e20f317f0c73a9e5c316913093c08cf5da21
shuhaoc <shuhaoc@qq.com>
2014-01-17 01:19:55 +0800

    添加bat脚本，用于更新其他依赖于此库的项目所使用的头文件和二进制库

---
+++ release.bat
@@ -0,0 +1,13 @@
+set MRL_ROOT=.
+
+msbuild /t:Rebuild /p:Configuration=Debug %MRL_ROOT%\mrl.sln
+msbuild /t:Rebuild /p:Configuration=Release %MRL_ROOT%\mrl.sln
+
+for /f %%i in (project_list.txt) do ^
+xcopy /e /y /i /f %MRL_ROOT%\include\mrl ..\%%i\include\mrl & ^
+xcopy /e /y /i /f %MRL_ROOT%\lib\Debug\*.lib ..\%%i\lib\Debug\ & ^
+xcopy /e /y /i /f %MRL_ROOT%\bin\Debug\*.dll ..\%%i\bin\Debug\ & ^
+xcopy /e /y /i /f %MRL_ROOT%\lib\Release\*.lib ..\%%i\lib\Release\ & ^
+xcopy /e /y /i /f %MRL_ROOT%\bin\Release\*.dll ..\%%i\bin\Release\
+
+pause
